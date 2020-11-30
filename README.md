de613ac0a62ca8de9d5b1c859457002ce8c4862a
2mill <yannick.dorn@gmail.com>
2020-11-30 13:49:31 -0500

    basic helper command, removed console logs from message events, updated README for node version

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Connect to my discord to get access to this bot.
 [Link!](https://discord.gg/F6jsM5m)
+
+
+## Dependancies
+Node v14.0.0 or later
---
+++ commands/help.js
@@ -0,0 +1,34 @@
+const { Message, MessageEmbed, DiscordAPIError } = require("discord.js");
+
+module.exports = {
+    name: 'help',
+    needClient: true,
+    description: 'A list of all the commands that are disposable to the user',
+    execute(message, args) {
+        const client = message.client;
+        let commands = client.commands.array();
+        const prefix = client.config.get('prefix');
+        const Discord = require('discord.js')
+        //Generate emebbed
+        let helpEmbed = new Discord.MessageEmbed()
+            .setTitle(`${message.client.user.username} Help`)
+            .setDescription('List of all commands')
+            .setColor('#52307C');
+        //add prefix field
+        helpEmbed.addField('Prefix:', prefix)
+        for (x of commands) {
+            helpEmbed.addField(x.name, x.description, true);
+        }
+        helpEmbed.setTimestamp();
+        message.channel.send(helpEmbed);
+
+        /**
+         * TODO: Allow users to get server specific config
+         * 
+         * This will require a database to be able to work on different servers
+         */
+
+        //stealing some code from github here quickly.
+        
+    }
+}
