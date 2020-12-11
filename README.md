c59aa30ed3d1e26d887a9d2a0e69d3a69032f821
2mill <yannick.dorn@gmail.com>
2020-12-10 22:03:35 -0500

    finished flipping command

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# Connect to my discord to get access to this bot.
+# Connect to my discord to get access to this bot. Feel free to ask questions about 
 [Link!](https://discord.gg/F6jsM5m)
 
 
 ## Dependancies
 Node v14.0.0 or later
+
+Currently deployed 
--- commands/flip.js
+++ commands/flip.js
@@ -3,10 +3,12 @@ module.exports = {
     description: 'Flips a 2 sided coin',    
     execute(message) {
         message.channel.send('Flipping coin...');
-
-
-
-
+        const x = Math.floor(Math.random() * 2) == 0;
+        if (x) {
+            message.channel.send('heads');
+        } else {
+            message.channel.send('tails');
+        }
     }
 
 
