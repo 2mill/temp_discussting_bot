ac531221e05cd9a63ccf4f56eea4f2cfa56835ab
2mill <yannick.dorn@gmail.com>
2020-12-20 11:55:33 -0500

    Refactored Purge command

---
+++ src/commands/purge.js
@@ -0,0 +1,30 @@
+module.exports = class Purge extends Commmands {
+    constructor(client) {
+        super({
+            name: 'purge',
+            shortDescription: 'Removes N messages previous to this message',
+            arguments: {
+                '--verbose': {
+                    shorthand: 'v',
+                    description: 'Verbose Description of the command',
+                },
+                '--reply': {
+                    shorthand: 'r',
+                    description: 'Used while purge replying to a message, Limit: 32',
+                }
+            },
+            requiremenets: {
+                permissions: {
+                    bot: ['MANAGE_MESSAGES'],
+                    user: ['MANAGE_MESSAGES'],
+                }
+            },
+            guildOnly: true,
+        });
+    }
+
+    getVerbose() {
+        verboseEmbedd = new client.MessageEmbed()
+            .setTitle(`${}`)
+    }
+}
