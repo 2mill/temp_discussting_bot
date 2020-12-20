374887eadc24b8deffef438a2b15dcca0d03dd83
2mill <yannick.dorn@gmail.com>
2020-12-20 11:54:18 -0500

    building new command super object

---
+++ src/Command.js
@@ -0,0 +1,16 @@
+module.exports = class Command {
+
+    constructor(options) {
+        this.name = options.name;
+        this.shortDescription = options.description;
+        this.usage = options.usage;
+        this.arguments = options.arguments;
+        this.requirements = options.requirements;
+        this.guildOnly = options.guildOnly;
+    }
+
+
+    
+
+
+}
