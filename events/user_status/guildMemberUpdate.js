a01a255c407300ae2af56cc3ca84f9c1f4af1975
Yannick Dorn <yannick.dorn@gmail.com>
2021-09-07 08:23:06 -0400

    View Comments in events commit

--- events/user_status/guildMemberUpdate.js
+++ events/user_status/guildMemberUpdate.js
@@ -12,7 +12,18 @@ module.exports = class Ready {
 			let logChannelNames = generateRoomNames(newGames, 'logs');
 			findGameLoggingChannels(logChannelNames, newMember.guild.channels).then(
 				channels => {
-					log_channels(logChannelNames, channels, newMember.guild.channels)
+					log_channels(logChannelNames, channels, newMember.guild.channels).then(
+						new_channels => {
+							//At this point, the program is able to
+							//not duplicated channels by accident.
+							//Need to write notes on my solution later.
+							for (let channel of new_channels) {
+								console.log(channel);
+								// channel.send("something")
+							}
+						}
+					)
+
 				}
 			)
 
@@ -24,18 +35,20 @@ module.exports = class Ready {
 }
 
 async function log_channels(loggingList, channels, channelManager) {
-	console.log(channels.keys())
-	for (let logChannel of loggingList) {
-
-		let channel = await channels.get(logChannel);
-		console.log(`${logChannel} -> ${channel}`)
-		if (!channel) {
-			console.log('Had to create a new channel');
-			channel = await channelManager.create(logChannel);
-		}
-		// console.log(channel)
+	// Figure out why it's mapping room ID instead of room name...
+	let fuckIt = Array.from(channels.values());
+	console.log(fuckIt)
+	let againFuck = [];
+	for (let channel of fuckIt) {
+		againFuck.push(channel.name);
 	}
-	
+	loggingList.forEach(element => {
+		if (!againFuck.includes(element)) {
+			fuckIt.push(channelManager.create(element));
+		}
+		
+	});
+	return fuckIt
 }
 
 let userGamesPlaying = user => {
