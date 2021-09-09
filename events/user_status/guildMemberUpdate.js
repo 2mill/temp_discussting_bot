d73537ad5f1ad539640d071d60459d520cf47e2e
2mill <yannick.dorn@gmail.com>
2021-09-09 13:44:47 -0400

    bot now makes category and places new channel into that category. Code needs major refactoring, but that will be a different branch

--- events/user_status/guildMemberUpdate.js
+++ events/user_status/guildMemberUpdate.js
@@ -1,6 +1,7 @@
 const chalk = require('chalk');
 const symbols = require('log-symbols');
-const { ActivityType, ChannelManager } = require('discord.js')
+const { ActivityType, ChannelManager, CommandInteractionOptionResolver } = require('discord.js')
+const GUILD_CATEGORY = "GUILD_CATEGORY"
 module.exports = class Ready {
     constructor() {
         this.name = 'presenceUpdate'
@@ -12,18 +13,17 @@ module.exports = class Ready {
 				activitesNamesOnly(userGamesPlaying(oldPresence)),
 				activitesNamesOnly(userGamesPlaying(newPresence)),
 			)
-			console.log(diff);
 			//No game update so no change.
 			if (diff.length === 0) return;
 			let loggingChannelsNames = generateRoomNames(diff, 'logs');
 			generateMissingChannels(loggingChannelsNames, newPresence.guild.channels).then(
 				channels => {
-					console.log(channels)
 					channels.forEach(channel => {
 						if (loggingChannelsNames.includes(channel.name)) {
-							channel.send(`USER: @${newPresence.user.username}\nSTARTED`);
+							console.log(channel)
+							channel.send(`@${newPresence.user.username} PLAYING`);
 						}
-					})
+					});
 				}
 			);
         }
@@ -33,22 +33,38 @@ module.exports = class Ready {
 }
 
 let diffNewGames = (oldPresenceGames, newPresenceGames) => newPresenceGames.filter(game => !oldPresenceGames.includes(game));
+//Checks to see if a log category has been created otherwise, makes one.
+
+let findCategory = (channels, categoryName) => {
+	let category = channels.filter(channel => channel.type === GUILD_CATEGORY && channel.name === categoryName);
+	return category.size === 0 ? false : category;
+}
+let createCategory = async (channelManager, categoryName) => channelManager.create(categoryName, {type: GUILD_CATEGORY});
+let missingChannels = async (existingChannels, channelList) => {
+	let foundChannels = existingChannels.filter(channel => channelList.includes(channel.name)).values();
+}
 
 
 async function generateMissingChannels(channelNames, channelManager) {
 	//This works, but I think can use some nice cleanup somewhere.
 	let channels = await channelManager.fetch()
 	let existingChannelNames = [];
+	let parent = findCategory(channels, "BOT-LOGS");
+
+	if (!parent) {
+		parent = await createCategory(channelManager, "BOT-LOGS");
+	}
 	//If the channel already exists, then push it into the channel list.
 	channels.forEach(channel => {
 		if (channelNames.includes(channel.name)) existingChannelNames.push(channel.name);
 	});
-	console.log(existingChannelNames);
-	//Otherwise, make a new channel.
+	//Otherwise, make a new channe//.
 	channelNames.forEach(channel => {
-		if (!existingChannelNames.includes(channel)) channelManager.create(channel);
+		if (!existingChannelNames.includes(channel)) channelManager.create(channel, {
+			parent: parent,
+		});
 	})
-	return channels;
+	return channelManager.fetch();
 
 }
 
