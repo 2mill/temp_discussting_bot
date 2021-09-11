c5a7147a2c14063e244456e1ed94f096a3c9cb43
Yannick Dorn <yannick.dorn@gmail.com>
2021-09-11 13:37:47 -0400

    started code cleanup

--- events/user_status/guildMemberUpdate.js
+++ events/user_status/guildMemberUpdate.js
@@ -1,23 +1,35 @@
 const chalk = require('chalk');
 const symbols = require('log-symbols');
 const { ActivityType, ChannelManager, CommandInteractionOptionResolver } = require('discord.js')
-const GUILD_CATEGORY = "GUILD_CATEGORY"
+const GUILD_CATEGORY = "GUILD_CATEGORY";
+const CATEGORY_NAME = "BOT-LOGS";
 module.exports = class Ready {
     constructor() {
         this.name = 'presenceUpdate'
         this.sender = false;
     }
+
+// This branch is going to clean up this code to be more efficient.
     eventHandler() {
         return function(oldPresence, newPresence) {
 			if (oldPresence.activities === null || newPresence.activities === null) return;
-          
-			let diff = diffNewGames(
+			const channelManager = newPresence.guild.channels;
+			const diff = diffNewGames(
 				activitesNamesOnly(userGamesPlaying(oldPresence)),
 				activitesNamesOnly(userGamesPlaying(newPresence)),
 			)
-			//No game update so no change.
 			if (diff.length === 0) return;
-			let loggingChannelsNames = generateRoomNames(diff, 'logs');
+			//No game update so no change.
+			const loggingChannelsNames = generateRoomNames(diff, 'logs');
+			channelManager.fetch().then(channels => {
+				let parent = findCategory(channels, CATEGORY_NAME);
+				if (!parent) {
+					parent = createCategory(chanels, CATEGORY_NAME);
+				}
+			});
+
+
+
 			console.log(loggingChannelsNames);
 			generateMissingChannels(loggingChannelsNames, newPresence.guild.channels).then(
 				channels => {
@@ -34,14 +46,52 @@ module.exports = class Ready {
     }
 }
 
+// The point of this function is to make one function call and then do all of the work
+async function gameLogger(diff, channelManager, loggingChannelsNames) {
+	const channels = await channelManager.fetch();
+	const parent = getCategory(channels, channelManager, CATEGORY_NAME);
+	//TODO: FIGURE OUT HOW TO MAP LOG CHANNEL NAMES TO ACTUAL CHANNELS AND FIX UNDFINEDs
+	const nameMap = channels.map(channel => channel.name)
+	//TODO: THEN RETURN A COLLECTION OF ONLY THE CHANNELS NEEDED
+}
+
 let diffNewGames = (oldPresenceGames, newPresenceGames) => newPresenceGames.filter(game => !oldPresenceGames.includes(game));
 //Checks to see if a log category has been created otherwise, makes one.
 
-let findCategory = (channels, categoryName) => {
-	let category = channels.filter(channel => channel.type === GUILD_CATEGORY && channel.name === categoryName);
-	return category.size === 0 ? false : category;
+
+
+/**
+ * 
+ * @param {A collection of channels} channels 
+ * @param {The guild channel manager} channelManager 
+ * @param {The name of the desired category} categoryName 
+ * @returns 
+ */
+async function getCategory(channels, channelManager, categoryName) {
+	let categories = findCategory(channels, categoryName);
+	if (!foundCategories) {
+		//Needs to be wrapped into an array to due the nature of findCategory.
+		categories = [await createCategory(channelManager, categoryName, force)];
+	}
+	//Even if findCategory finds multiple categories, it will still return the first one it finds.
+	return categories.pop();
 }
-let createCategory = async (channelManager, categoryName) => channelManager.create(categoryName, {type: GUILD_CATEGORY});
+/**
+ * 
+ * @param {The given channels} channels 
+ * @param {The category name that is wanted to find} categoryName 
+ * @returns {Array if found else false boolean}
+ */
+let findCategory = (channels, categoryName) => {
+	let category = channels.filter(channel => isCategory(channel) && channel.name === categoryName);
+	//Due to the nature of Discord.js returning a collection instead of an array with filter.
+	//It's needed that we collect the actual values instead. Hence .values() being used here.
+	return category.size === 0 ? false : category.values();
+}
+
+let isCategory = (channel) => channel.type === GUILD_CATEGORY;
+//If force is not set, and the category already exists, then the new category will not be made.
+let createCategory = async (channelManager, categoryName, force=false) => findCategory(channelManager, categoryName) && force ? channelManager.create(categoryName, {type: GUILD_CATEGORY}) : force;
 let missingChannels = async (existingChannels, channelList) => {
 	let foundChannels = existingChannels.filter(channel => channelList.includes(channel.name)).values();
 }
